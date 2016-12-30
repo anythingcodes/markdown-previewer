a15120270429e9348dfab6672b392f13a424ddd3
Liz Shaw <liz.shaw.medtouch@gmail.com>
2016-12-29 19:27:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# markdown-previewer
+
+**[Demo](https://preview-markdown.herokuapp.com)**
+
+A Markdown editor with preview that allows users to login and share Markdown documents. Made with the power of Meteor and React.
+
+## Instructions
+
+Navigate to the `meteor-react-markdown-preview` folder in the command line. Make sure you have node installed on your machine, then run `npm i` to install node modules. Navigate to the `meteor-react-markdown-preview/app` folder in the command line, make sure you have Meteor installed on your machine, and run `meteor` from that directory. You will be given the app's URL.
---
+++ app/client/components/bins/bins_main.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from '../../../imports/collections/bins';
+import BinsEditor from './bins_editor';
+import BinsViewer from './bins_viewer';
+import BinsShare from './bins_share';
+
+class BinsMain extends Component {
+    render() {
+        if(!this.props.bin) {
+            return <div>Loading...</div>;
+        }
+        return (
+            <div>
+                <BinsEditor bin={this.props.bin} />
+                <BinsViewer bin={this.props.bin} />
+                <BinsShare bin={this.props.bin} />
+            </div>
+        )
+    }
+}
+
+export default createContainer((props) => {
+    const { binId } = props.params;
+    Meteor.subscribe('sharedBins');
+
+    Meteor.subscribe('bins');
+    // BinList subscription only kicks in when first
+    // on BinList route, so need to re-subscribe
+    return { bin: Bins.findOne(binId) };
+}, BinsMain);
